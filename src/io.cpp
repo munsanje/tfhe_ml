e3efcb655d5ec720197dee2a16980e803b4050b8
Munsanje Mweene <mmweene15@gmail.com>
2018-09-05 10:56:28 +0200

    Working csv write

--- src/io.cpp
+++ src/io.cpp
@@ -18,13 +18,23 @@ vector<vector<int>> readFile(char* filepath) {
     file.close();
   }
   else {
-    cout << "Error: failed to open file.";
+    cout << "Error: failed to open file." << endl;
   }
   read_to;
 }
 
-void writeFile(std::vector<vector<int>> write_from, char* filepath) {
-
+int writeFile(std::vector<vector<int>> write_from, char* filepath) {
+  ofstream file(filepath);
+  if(file.is_open()) {
+    for(vector<vector<int>>::iterator it = write_from.begin(); it != write_from.end(); it++) {
+      file << rowToString(*it) << "\n";
+    }
+    file.close();
+  }
+  else {
+    cout << "Error: failed to open file." << endl;
+    return -1;
+  }
 
 }
 
@@ -42,11 +52,25 @@ vector<int> parseLine(string line, char delimiter) {
   return parsed;
 }
 
+string rowToString(vector<int> row, char delimiter) {
+  string line = "";
+  for(int i: row) {
+    line += to_string(i) + delimiter;
+  }
+  line.erase(line.length()-1);
+  return line;
+
+}
+
 int main() {
   string line = "32,43,3,24,3";
-  // cout << line.length() << endl;
-  vector<int> parsed = parseLine(line, ',');
-  for(vector<int>::iterator it = parsed.begin(); it != parsed.end(); it++)
-    cout << " " << *it;
+  vector<int> parsed = parseLine(line);
+  for(int i: parsed)
+    cout << " " << i;
   cout << endl;
+  cout << rowToString(parsed) << endl;
+  vector<vector<int>> vec;
+  vec.push_back(parsed);
+  vec.push_back(parsed);
+  cout << writeFile(vec, "testcsv.csv") << endl;
 }
--- src/io.hpp
+++ src/io.hpp
@@ -2,8 +2,8 @@
 
 #include <vector>
 
-
 // COULD use a template but the use case is quite specific
 std::vector<std::vector<int>> readFile(char* filepath);
-void writeFile(std::vector<vector<int>> write_from, char* filepath);
-std::vector<int> parseLine(std::string line, char delimiter);
+int writeFile(std::vector<std::vector<int>> write_from, char* filepath);
+std::vector<int> parseLine(std::string line, char delimiter=',');
+std::string rowToString(std::vector<int> row, char delimiter=',');
