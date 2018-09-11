dba5561773a1c5a37490fe7f6eced322f8a803bd
Munsanje Mweene <mmweene15@gmail.com>
2018-09-12 00:52:45 +0200

    Removed parallelism on decrypt due to corruption

--- src/encryption.hpp
+++ src/encryption.hpp
@@ -6,26 +6,28 @@ Implements higher level encryption functions to make using tfhe easier
 #include <tfhe/tfhe.h>
 #include <tfhe/tfhe_io.h>
 #include <vector>
+#include "omp_constants.hpp"
 
+
+using namespace std;
 /*Encrypt plaintext sample given secret key*/
 template<typename T>
 
-void encrypt(LweSample* cipher, T& plaintext, const TFheGateBootstrappingSecretKeySet* sk) {
+void encrypt(LweSample* cipher, T plaintext, const TFheGateBootstrappingSecretKeySet* sk) {
   uint8_t type_size = sizeof(T) * 8;
   for(int i = 0; i < type_size; i++) {
     bootsSymEncrypt(&cipher[i], (plaintext >> i) & 1, sk);
   }
 }
 
-// template<typename T>
-// void encrypt(vector<LweSample*> cipher, vector<T>& plaintext, const TFheGateBootstrappingSecretKeySet* sk) {
-//   uint8_t type_size = sizeof(T) * 8;
-//   for(int i = 0; i < type_size; i++) {
-//     bootsSymEncrypt(&cipher[i], (*plaintext >> i) & 1, sk);
-//   }
-// }
-
-
+template<typename T>
+void encrypt(LweSample** cipher, vector<T>& plaintext, const TFheGateBootstrappingSecretKeySet* sk) {
+  uint8_t type_size = sizeof(T) * 8;
+  #pragma omp parallel for num_threads(NUM_THREADS)
+  for(int i = 0; i < plaintext.length(); i++) {
+    encrypt(cipher[i], plaintext[i], sk);
+  }
+}
 
 /*Decrypt cipher given secret key*/
 template<typename T>
@@ -34,6 +36,19 @@ T decrypt(LweSample* cipher, const TFheGateBootstrappingSecretKeySet* sk) {
   T plaintext = 0;
   for(int i = 0; i < type_size; i++) {
     plaintext |= bootsSymDecrypt(&cipher[i], sk) << i;
+  }
+  return plaintext;
+}
+
+template<typename T>
+vector<T> decrypt(LweSample** cipher, int length, const TFheGateBootstrappingSecretKeySet* sk) {
+  uint8_t type_size = sizeof(T) * 8;
+  vector<T> plaintext(length, 0);
+  #pragma omp parallel for num_threads(NUM_THREADS)
+  for(int i = 0; i < length; i++) {
+    for(int j = 0; j < type_size; j++) {
+      plaintext[i] |= bootsSymDecrypt(&cipher[i][j], sk) << j;
+    }
   }
   return plaintext;
 }
